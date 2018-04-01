f112fb2d90679db5d5c3c5c84fcc6fbf6178aece
李婷婷 <tina1998612@users.noreply.github.com>
2018-04-01 17:41:59 +0800

    Create DataManipulation.sol

---
+++ examples/basic/DataManipulation.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.8;
+
+contract DataManipulation{
+    uint public x = 1;
+    uint public y = 2;
+    
+    // (x, y) = (y, x);
+    
+    function demo(){
+        assert(x == 1 && y == 2);
+        (y, x) = (x, y);
+        assert(x == 2 && y == 1);
+    }
+    
+}
