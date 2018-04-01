e8c32eb2953faf234c5c51911ae38364f9fd61c7
李婷婷 <tina1998612@users.noreply.github.com>
2018-04-01 18:08:08 +0800

    Update MultiContract.sol

--- examples/medium/MultiContract.sol
+++ examples/medium/MultiContract.sol
@@ -1,12 +1,14 @@
 pragma solidity ^0.4.8;
 
 contract A{
-    function greet() returns (string){
-        return "Hi";
+    function greet(){
     }
 }
 
 contract B{
     A sampleA = new A();
-    A.greet();
+    
+    function greetByB(){
+        sampleA.greet();
+    }
 }
