3059f487403f8b32d08f82819b0addec7ab76c11
Hu Yao-Chieh <yhuag@connect.ust.hk>
2018-09-16 09:22:22 +0800

    add Template

---
+++ examples/basic/Template.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.24;    // 版本宣告
+
+/* 合約本體 */
+contract Template {
+
+    /* 變數宣告 */
+    uint number = 0;
+
+    // ... 更多變數
+
+
+
+
+    /* 函數定義 */
+    function getNumber() public view returns (uint) {
+        return number;
+    }
+
+    // ... 更多函數 
+
+    
+}
