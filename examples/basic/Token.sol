e4bbedc783a316a06aae530f500d1938e56bd857
李婷婷 <tina1998612@users.noreply.github.com>
2019-03-27 23:20:54 -0700

    Update Token.sol

--- examples/basic/Token.sol
+++ examples/basic/Token.sol
@@ -3,18 +3,19 @@
  * Orginal source: https://www.ethereum.org/token
 */
 
-pragma solidity ^0.4.21;
+pragma solidity 0.4.24;
 
 contract Token {
     mapping (address => uint256) public balances;
     
-    function Token(uint256 _initialSupply) public {
+    constructor(uint256 _initialSupply) public {
         balances[msg.sender] = _initialSupply;
     }
-
+   
     function transfer(address _to, uint256 _value) public {
         require(balances[msg.sender] >= _value);
         require(balances[_to] + _value >= balances[_to]);   // Avoid overflows
+        
         balances[msg.sender] -= _value;
         balances[_to] += _value;
     }
