e7dd223afaee7a3f373ef16991515577d9c1647c
Hu Yao-Chieh <yhuag@connect.ust.hk>
2019-06-23 18:26:26 -0700

    add SimpleToken

---
+++ examples/basic/SimpleToken.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+
+/**
+ * @title SimpleToken
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `ERC20` functions.
+ */
+contract SimpleToken is ERC20, ERC20Detailed {
+
+  uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** 18);
+
+  /**
+   * @dev Constructor that gives msg.sender all of existing tokens.
+   */
+  constructor() public ERC20Detailed("TaiyakiToken", "TYK", 18) {
+    _mint(msg.sender, INITIAL_SUPPLY);
+  }
+
+}
